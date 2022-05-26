95a6b3bce25c1d4f121dd5f2bbb8b9f0c3a22902
Lauti Posti <lautaropostiglione@gmail.com>
2022-05-26 20:52:10 -0300

    Con arreglos

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -113,7 +113,8 @@ function esEntero(numero) {
   // Tu código:
   if (numero % 1 === 0) {
     return true 
-  } else {
+  } 
+  else {
     return false 
   }
 }
@@ -123,14 +124,14 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
-  if (numero % 3 === 0) {
-    return "fizz"
+  if (numero % 15 === 0) {
+    return "fizzbuzz"
   }
   else if (numero % 5 === 0) {
     return "buzz"
   }
-  else if (numero % 5 === 0 && numero % 3 === 0) {
-    return "fizzbuzz"
+  else if (numero % 3 === 0) {
+    return "fizz"
   } 
   else {
     return numero
@@ -144,21 +145,11 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-  if (num1 > num2 && num1 > num3 && num1 > 0) {
-    return "Número 1 es mayor y positivo"
-  }
-   else if (num1 < 0 || num2 < 0 || num3 < 0) {
-     return "Hay negativos"
-   } 
-   else if (num3 > num1 && num3 > num2 ) {
-     return numero + 1
-   }
-   else if (num1 === 0 || num2 === 0 || num3 === 0) {
-     return "Error"
-   } 
-    else {
-     return false
-   }
+  if (num1 === 0 || num2 === 0 || num3 === 0) return "Error";
+  if (num1 < 0 || num2 < 0 || num3 < 0) return "Hay negatvos";
+  if (num1 > num2 && num1 > num3) return "Número 1 es mayor y positivo"
+  if (num3 > num1 && num3 > num2) return num3 + 1
+  return false 
 }
 
 function esPrimo(numero) {
@@ -167,6 +158,18 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if (numero < 2 ) {
+    return false;
+  }
+  if (numero === 2) {
+    return true;
+  }
+  for (var i = 2; i < numero; i++ ) {
+    if (numero % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 function esVerdadero(valor){
@@ -213,8 +216,8 @@ function doWhile(numero) {
     i = i + 1;
     a = a + 5;
   } 
-  while (a < 8) {
-    return a
+  while(i < 8) {
+    return a;
   }
 }
 
